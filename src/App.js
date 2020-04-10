c3b31aee3d9579148ffe46f4be3e47ec0dd610b4
Willian Amaral <willian.amaral@outlook.com.br>
2020-04-10 16:13:32 -0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from 'pages/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Home = () => (
+  <>
+    <Helmet>
+      <title>Home Page</title>
+      <meta name='description' content='Home Page' />
+    </Helmet>
+    <div>Home page</div>
+  </>
+);
+
+export default Home;

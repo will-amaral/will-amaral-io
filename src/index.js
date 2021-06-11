e1362027280062e24b26e0749c8e53da82e26b5b
Willian Amaral <willian.amaral@outlook.com.br>
2021-06-11 13:34:47 -0300

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from 'react-snapshot';
+import { hydrate, render } from 'react-dom';
 import 'assets/sass/main.scss';
 import App from 'App';
 import * as serviceWorker from 'serviceWorker';
 
-render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.querySelector('#root')
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
